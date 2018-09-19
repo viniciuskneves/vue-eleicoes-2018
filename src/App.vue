f3e41174943e3fd77109f907be7411b8f0861bdb
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-09-19 14:35:24 -0300

    Import AppFooter

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,31 @@
   <div>
     <AppHeader />
     <AppBody />
+    <AppFooter />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppBody from './components/AppBody.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     AppBody,
+    AppFooter,
   },
   created() {
     this.$store.dispatch('FETCH_DATA');
   },
 };
 </script>
+
+<style>
+body {
+  max-width: 750px;
+  margin: 0 auto;
+}
+</style>
