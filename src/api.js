f455830c1e8edc95e2aaafa22ee54066280b55b7
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-09-19 14:35:53 -0300

    GetPresidente API - Image

--- src/api.js
+++ src/api.js
@@ -3,4 +3,8 @@ export default {
     return fetch('http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2018/BR/2022802018/1/candidatos')
       .then(response => response.json());
   },
+  getPresidente(id) {
+    return fetch(`http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/2018/BR/2022802018/candidato/${id}`)
+      .then(response => response.json());
+  },
 };
