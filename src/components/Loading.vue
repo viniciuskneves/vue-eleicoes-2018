45ebd10ccae1657911ed23af264a8a09f9bf650f
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-09-19 14:37:16 -0300

    Update Loading component

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Loading</h1>
+  <div></div>
 </template>
 
 <script>
@@ -7,3 +7,28 @@ export default {
   name: 'Loading'
 }
 </script>
+
+<style scoped>
+div {
+  margin: 0 auto;
+  border: 2rem solid #fff;
+  border-radius: 50%;
+  border-top: 2rem solid #002776;
+  border-right: 2rem solid #009c3b;
+  border-bottom: 2rem solid #ffdf00;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
