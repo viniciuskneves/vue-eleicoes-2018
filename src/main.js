be9ca9a87e242e46686e555cd2f847469176f9c3
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-09-19 13:42:06 -0300

    Remove Vuetify dependency

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import 'vuetify/dist/vuetify.min.css';
-
 import Vue from 'vue';
-import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
