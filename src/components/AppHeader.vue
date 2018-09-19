4b408850e0ce5a435a89455c31c12fe762e264db
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-09-19 14:36:39 -0300

    Update Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>HEADER</h1>
+  <h1>
+    <span>Eleições 2018</span>
+  </h1>
 </template>
 
 <script>
@@ -8,6 +10,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1 {
+  overflow: hidden;
+  background-image: linear-gradient(to bottom,#2ed72a 0%,#2ed72a 40%,#ebff39 40%,#ebff39 80%,#fff 80%,#fff 100%);
+  font-size: 4rem;
+}
 
+span {
+  box-shadow: 10px 0 15px 20px #fff;
+  background: #fff;
+}
 </style>
