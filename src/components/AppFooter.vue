c7130656eddfde6b095c98c6d6491a6216a49e0a
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-09-20 14:22:00 -0300

    Update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>APPFOOTER</h1>
+  <footer>
+    <p>As informações contidas nessa página foram retiradas do <a href="http://divulgacandcontas.tse.jus.br" target="_blank">Site Oficial do TSE</a>.</p>
+    <p>Este projeto tem o <a href="https://github.com/viniciuskneves/vue-eleicoes-2018">código aberto</a> sob a <a href="https://pt.wikipedia.org/wiki/Licen%C3%A7a_MIT">Licença MIT</a>.</p>
+  </footer>
 </template>
 
 <script>
@@ -8,6 +11,16 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+footer {
+  text-align: center;
+}
 
+p {
+  color: #aaa;
+}
+
+a {
+  color: #777;
+}
 </style>
