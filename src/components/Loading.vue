141acd341adaf524cea7f55bf00ffbd24ff19ca6
viniciuskneves <kneves.vinicius@gmail.com>
2018-09-22 13:15:24 -0300

    Update loading style

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,34 +1,50 @@
+<!-- Based on https://codepen.io/Manoz/pen/pydxK -->
 <template>
-  <div></div>
+  <div class="load">
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Loading'
+  name: 'Loading',
 }
 </script>
 
 <style scoped>
-div {
-  margin: 0 auto;
-  border: 2rem solid #fff;
-  border-radius: 50%;
-  border-top: 2rem solid #002776;
-  border-right: 2rem solid #009c3b;
-  border-bottom: 2rem solid #ffdf00;
-  width: 120px;
-  height: 120px;
-  -webkit-animation: spin 2s linear infinite;
-  animation: spin 2s linear infinite;
+.load {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 50px;
 }
 
-@-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
+.line {
+  margin: 0 3px;
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  border-radius: 15px;
+  background-color: #4b9cdb;
 }
 
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+.load .line:nth-last-child(1) {
+  background-color: #002776;
+  animation: loading .6s .1s linear infinite;
+}
+.load .line:nth-last-child(2) {
+  background-color: #009c3b;
+  animation: loading .6s .2s linear infinite;
+}
+.load .line:nth-last-child(3) {
+  background-color: #ffdf00;
+  animation: loading .6s .3s linear infinite;
+}
+
+@keyframes loading {
+    0 {transform: translate(0,0);}
+    50% {transform: translate(0,15px);}
+    100% {transform: translate(0,0);}
 }
 </style>
