7d25853967f175c2b2396ca031bbc90067b3d635
Vinicius Kiatkoski Neves <vinicius.neves@chaordicsystems.com>
2018-10-02 16:33:53 -0300

    Enable vue-router (#1)
    * Use vue-router to /presidentes
    
    * Handle Surge SPA redirect

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,26 +1,20 @@
 <template>
   <Loading v-if="loading" />
-  <CandidatosList v-else />
+  <router-view v-else />
 </template>
 
 <script>
 import { mapState } from 'vuex';
 
 import Loading from './Loading.vue';
-import CandidatosList from './CandidatosList.vue';
 
 export default {
   name: 'AppBody',
   components: {
     Loading,
-    CandidatosList,
   },
   computed: mapState({
     loading: state => !state.candidatos.length,
   }),
 };
 </script>
-
-<style>
-
-</style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import CandidatosList from './components/CandidatosList.vue';
 
 Vue.use(Router);
 
@@ -7,5 +8,17 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      redirect: '/presidentes',
+    },
+    {
+      path: '/presidentes',
+      component: CandidatosList,
+    },
+    {
+      path: '*',
+      redirect: '/',
+    },
   ],
 });
