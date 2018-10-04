8f54073180c58c663ed73965db3fef654831f04c
vini23hot <vini23hot@gmail.com>
2018-10-03 21:41:51 -0300

    Fixed indentation

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,9 +1,11 @@
 <template>
-    <nav class="menu">
-        <div :key="route.path" v-for="route in routes">
-            <router-link class="route" :to="route.path">{{ route.name }}</router-link>
-        </div>
-    </nav>
+  <nav class="menu">
+    <div :key="route.path" v-for="route in routes">
+      <router-link class="route" :to="route.path">
+        {{ route.name }}
+      </router-link>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -17,10 +19,7 @@ export default {
   created() {
     this.$router.options.routes.forEach((route) => {
       if (route.name) {
-        this.routes.push({
-          name: route.name,
-          path: route.path,
-        });
+        this.routes.push({ name: route.name, path: route.path });
       }
     });
   },
@@ -29,31 +28,27 @@ export default {
 
 <style scoped>
 .menu {
-    margin-bottom: 3em;
-    list-style: none;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
+  margin-bottom: 3em;
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 .menu div {
-    margin: 0;
-    padding: 0;
-    margin: 20px 10px;
+  margin: 0;
+  padding: 0;
+  margin: 20px 10px;
 }
 .route {
-    border-width: 1px 1px 1px 1px;
-    border-style: solid;
-    border-color: #e6e6e6;
-    height: 100%;
-    padding: 10px 20px;
-    /* margin: 20px 40px; */
-    transition: all 0.3s ease;
-    font-size: 1.2rem;
-    color: black;
-    text-decoration: none;
-    text-transform: uppercase;
+  border: 1px solid #e6e6e6;
+  height: 100%;
+  padding: 10px 20px;
+  transition: all 0.3s ease;
+  color: black;
+  text-decoration: none;
+  text-transform: uppercase;
 }
 .route:hover {
-    background-color: #fafafa;
+  background-color: #fafafa;
 }
 </style>
