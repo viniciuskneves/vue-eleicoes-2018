bf4bcf0734580b91a75458a2c990437a78dc274d
vini23hot <vini23hot@gmail.com>
2018-10-05 16:02:55 -0300

    Fixed conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <app-header/>
-    <app-menu />
+    <app-menu/>
     <router-view/>
     <app-footer/>
   </div>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import ThePresidents from '@/views/ThePresidents.vue';
 
 Vue.use(Router);
 
