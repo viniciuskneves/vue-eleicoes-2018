2b9aa9443d184cea86bb9372b76b1955fa4f5817
Michael Santillán <me@michaelsantillan.com>
2018-10-05 20:44:57 -0300

    Decouple data fetch in API close #12

--- src/store/presidents/actions.js
+++ src/store/presidents/actions.js
@@ -3,9 +3,9 @@ import { getCandidatesList, getCandidate } from '@/api';
 const fetchPayload = async ({ commit }) => {
   try {
     const { candidatos } = await getCandidatesList();
+    commit('SET_CANDIDATES', candidatos);
     const promises = candidatos.map(candidato => getCandidate(candidato.id));
     const candidatesPayload = await Promise.all(promises);
-
     commit('SET_CANDIDATES', candidatesPayload);
   } catch (e) {
     console.error(e);
